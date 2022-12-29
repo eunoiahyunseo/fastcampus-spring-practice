29c82d26dce86e93ab67d1e107e9983f2646bd0f
eunoiahyunseo <heart20021010@gmail.com>
2022-12-29 23:27:24 +0900

    Config #1 - spring configuration
    기초 설정
    루트 패키지 이하에 빈이 있는지 찾는다.

---
+++ src/main/java/com/eunoiahyunseo/springpractice/config/Config.java
@@ -0,0 +1,12 @@
+package com.eunoiahyunseo.springpractice.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author ihyeonseo
+ */
+@ComponentScan("com.eunoiahyunseo.springpractice")
+@Configurable
+public class Config {
+}
